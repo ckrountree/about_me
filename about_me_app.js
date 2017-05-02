df54efdcb07568b0197378ea8601e1102e8d9b74
ckrountree <jmail0207@gmail.com>
2017-05-02 16:08:06 -0700

    4th addition js

--- about_me_app.js
+++ about_me_app.js
@@ -7,10 +7,31 @@ var name=prompt('Is Kate my first name?');
 var nameUpper=name.toUpperCase();
 console.log(nameUpper);
 
-//If the answer to 'first name Kate' is no, output "Booyah!" in an element with id="name"
-if (nameUpper === 'No' || nameUpper === 'N' || nameUpper === 'NO' || nameUpper === 'no') {
+//If the answer to 'first name Kate' is no, output "Booyah!" in an element with id="name"//
+    if (nameUpper === 'No' || nameUpper === 'N' || nameUpper === 'NO' || nameUpper === 'no') {
     document.getElementById('name').innerHTML = 'Booyah!';
-}
-else{
-    alert('Oh, no!  You don\'t know me at all');
-}
+    }
+    else{
+        alert('Oh, no!  You don\'t know me at all');
+    }
+console.log('Q1? ' + name);
+
+//If the answer to 'age' is yes, output "What are you trying to say?" //
+var age=prompt('Am I over 21 years old?');
+    if (age.toUpperCase () === 'y' || === 'yes') {
+        alert('What are you trying to say?');
+    }
+    else{
+        alert('Thank you!');
+    }
+    console.log('Q2? ' + age);
+
+//If the answer to 'age' is yes, output "What are you trying to say?" //
+var soccer=prompt('Was I once a soccer player?');
+    if (soccer.toUpperCase () === 'y' || === 'yes') {
+        alert('GOAL!?');
+    }
+    else{
+        alert('Foul!');
+    }
+    console.log('Q2? ' + age);

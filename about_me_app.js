6d02b4626c6aa346c716a3ff47941f3fb09e1b1d
ckrountree <jmail0207@gmail.com>
2017-05-02 15:15:19 -0700

    2nd addition

--- about_me_app.js
+++ about_me_app.js
@@ -1,3 +1,5 @@
+// this script asks if Kate is my first name //
 var name=prompt('Is Kate my first name?');
+//this variable allows either letter case to be allowed and converted to uppercase//
 var nameUpper=name.toUpperCase();
 console.log(nameUpper);

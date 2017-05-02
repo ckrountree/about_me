0f0f61161551cceffb6df4358b611f7ad429254f
ckrountree <jmail0207@gmail.com>
2017-05-02 15:56:17 -0700

    3rd addition js

--- about_me_app.js
+++ about_me_app.js
@@ -1,5 +1,16 @@
+'use strict'
+
 // this script asks if Kate is my first name //
 var name=prompt('Is Kate my first name?');
+
 //this variable allows either letter case to be allowed and converted to uppercase//
 var nameUpper=name.toUpperCase();
 console.log(nameUpper);
+
+//If the answer to 'first name Kate' is no, output "Booyah!" in an element with id="name"
+if (nameUpper === 'No' || nameUpper === 'N' || nameUpper === 'NO' || nameUpper === 'no') {
+    document.getElementById('name').innerHTML = 'Booyah!';
+}
+else{
+    alert('Oh, no!  You don\'t know me at all');
+}
